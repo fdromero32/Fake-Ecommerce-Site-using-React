b8a6e87338f4957f4c2d288a222cdca8b6d50f22
Danny Romero <fdromero32@gmail.com>
2020-09-30 19:04:47 -0700

    Updated Toggle Function - kinda makes sense now

--- src/Context.js
+++ src/Context.js
@@ -18,15 +18,11 @@ function ContextProvider({ children }) {
   function toggleFavorite(id) {
     const updatedArray = photos.map((photo) => {
       if (id === photo.id) {
-        console.log(id);
-        console.log(!photo.isFavorite);
-        return {
-          ...photo,
-          isFavorite: !photo.isFavorite,
-        };
+        photo.isFavorite = !photo.isFavorite;       
       }
-      return photo;
-    });
+      return photo
+    })
+    setPhotos(updatedArray)
   }
 
   return (
